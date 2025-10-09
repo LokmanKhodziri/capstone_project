cc7e55e1400e1178af59233285a337f049a9608f
lokmankhodziri <lokmankhodziri@gmail.com>
2025-10-09 22:18:08 +0800

    Adding monthly filter on expense list

--- frontend/src/components/ExpenseList.js
+++ frontend/src/components/ExpenseList.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ExpenseService from "../services/expense.service";
+import { FaFilter, FaCalendarAlt } from "react-icons/fa";
 
 const ExpenseList = () => {
   const [expenses, setExpenses] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [categoryFilter, setCategoryFilter] = useState("All");
+  const [monthFilter, setMonthFilter] = useState("All");
 
   useEffect(() => {
     ExpenseService.getAll().then(
@@ -33,7 +35,14 @@ const ExpenseList = () => {
     .filter(
       (expense) =>
         categoryFilter === "All" || expense.category === categoryFilter
-    );
+    )
+    .filter((expense) => {
+      if (monthFilter === "All") {
+        return true;
+      }
+      const expenseMonth = new Date(expense.date).getMonth();
+      return expenseMonth === parseInt(monthFilter);
+    });
 
   const categories = [
     "All",
@@ -42,6 +51,22 @@ const ExpenseList = () => {
     "Utilities",
     "Entertainment",
     "Other",
+  ];
+
+  const months = [
+    "All",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
   ];
 
   return (
@@ -54,26 +79,50 @@ const ExpenseList = () => {
       </div>
 
       <div className="row mb-4">
-        <div className="col-md-6">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search by description..."
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
+        <div className="col-lg-4 col-md-6 mb-3">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by description..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
         </div>
-        <div className="col-md-6">
-          <select
-            className="form-control"
-            value={categoryFilter}
-            onChange={(e) => setCategoryFilter(e.target.value)}>
-            {categories.map((category) => (
-              <option key={category} value={category}>
-                {category}
-              </option>
-            ))}
-          </select>
+        <div className="col-lg-4 col-md-6 mb-3">
+          <div className="input-group">
+            <span className="input-group-text">
+              <FaFilter />
+            </span>
+            <select
+              className="form-select"
+              value={categoryFilter}
+              onChange={(e) => setCategoryFilter(e.target.value)}>
+              {categories.map((category) => (
+                <option key={category} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className="col-lg-4 col-md-6 mb-3">
+          <div className="input-group">
+            <span className="input-group-text">
+              <FaCalendarAlt />
+            </span>
+            <select
+              className="form-select"
+              value={monthFilter}
+              onChange={(e) => setMonthFilter(e.target.value)}>
+              {months.map((month, index) => (
+                <option key={month} value={index === 0 ? "All" : index - 1}>
+                  {month}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       </div>
 
@@ -81,7 +130,7 @@ const ExpenseList = () => {
         {filteredExpenses.length > 0 ? (
           filteredExpenses.map((expense) => (
             <div key={expense.id} className="col-md-6 col-lg-4 mb-4">
-              <div className="card h-100">
+              <div className="card h-10.0">
                 <div className="card-body d-flex flex-column">
                   <h5 className="card-title">{expense.description}</h5>
                   <p className="card-text text-success fs-4">{`RM${expense.amount.toFixed(
